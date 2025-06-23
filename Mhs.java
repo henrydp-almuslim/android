fcdc65eb0a47eef71f5773b2013e8d80f21adfd6
henrydp-almuslim <henry.dp@almuslim.ac.id>
2025-06-23 21:23:56 +0700

    Create Mhs.java

---
+++ Mhs.java
@@ -0,0 +1,30 @@
+package com.example.itscollegerlist;
+
+public class Mhs {
+    private int photo;
+    private String nim, nama;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
